058c17d6194c66fb7e48f43b6c8f14f885c1b96d
coegle18 <coegle18@gmail.com>
2021-10-30 22:24:26 +0800

    修改了文件目录

--- part1/exercise1.1-1.5/src/App.js
+++ part1/exercise1.1-1.5/src/App.js
@@ -17,6 +17,13 @@ const App = () => {
       }
     ]
   }
+  const arto = {
+    name: 'Arto Hellas',
+    greet: function() {
+      console.log('hello, my name is ' + this.name)
+    },
+  }
+  setTimeout(arto.greet.bind(arto), 1000)
   const Header = (props) => (
     <>
     <h1>{props.course}</h1>
