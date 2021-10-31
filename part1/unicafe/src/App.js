dd56bee91972e1b3074de408746174bc3329eebb
coegle18 <coegle18@gmail.com>
2021-10-31 21:33:07 +0800

    完成exercise1.6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,5 +1,17 @@
 import React, { useState } from 'react'
 
+const Title = ({caption}) => (
+  <h2>{caption}</h2>
+)
+
+const Button = ({caption, onClick}) => (
+  <button onClick={onClick}>{caption}</button>
+)
+
+const Statistic = ({caption, num}) => (
+  <div>{caption} {num}</div>
+)
+
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
@@ -8,7 +20,14 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <Title caption='give feedback' />
+      <Button caption='good' onClick={() => {setGood(good + 1)}}/>
+      <Button caption='neutral' onClick={() => {setNeutral(neutral + 1)}}/>
+      <Button caption='bad' onClick={() => {setBad(bad + 1)}}/>
+      <Title caption='statistics' />
+      <Statistic caption='good' num={good} />
+      <Statistic caption='neutral' num={neutral} />
+      <Statistic caption='bad' num={bad} />
     </div>
   )
 }
