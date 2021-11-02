f435c8ed8eb697cdb34a50fb3ca630087d31d34a
coegle18 <coegle18@gmail.com>
2021-11-02 19:19:44 +0800

    完成exercise2.4

--- part2/courseInformation/src/App.js
+++ part2/courseInformation/src/App.js
@@ -5,7 +5,7 @@ const Part = ({part}) => {
   )
 }
 
-const Header = ({header}) => <h1>{header}</h1>
+const Header = ({header}) => <h2>{header}</h2>
 
 const Total = ({total}) => <p><b>total of {total} exercises</b></p>
 
@@ -30,31 +30,55 @@ const Course = ({course}) => {
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
-
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+  const coursesEle = courses.map((it) => <Course key={it.id} course={it}/>)
   return (
     <div>
-    <Course course={course}/>
+    <h1>Web development curriculum</h1>
+    {coursesEle}
     </div>
   )
 }
