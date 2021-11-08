f06798db168e1442aa97cd624b1e2864074eea53
coegle18 <coegle18@gmail.com>
2021-11-08 16:44:21 +0800

    完成exercise2.6

---
+++ part2/thePhoneBook/src/App.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+  const onSubmit = (event) => {
+    event.preventDefault()
+    setPersons(persons.concat({name: newName}))
+    setNewName('')
+  }
+  const onInputChange = (event) => {
+    setNewName(event.target.value)
+  }
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={onSubmit}>
+        <div>
+          name: <input value={newName} onChange={onInputChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => <p key={person.name}>{person.name}</p>)}
+    </div>
+  )
+}
+
+export default App
